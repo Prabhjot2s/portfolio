e52a0c436ac6f2b3d4ce846cdf6316c3b5aceb3c
prabh <pschhabra22@gmail.com>
2023-06-27 16:08:01 -0400

    Rendered title ,image and description of all projects.

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import streamlit as st
+import pandas as pd
+
+
+
+
 st.set_page_config(layout='wide')
-col1,col2=st.columns(2)
+col1,col2=st.columns([2,2])
+
+def structure(heading,description,images,sourcecode='qq'):
+    st.title(heading)
+    st.write(description)
+    st.image(f"images/{images}")
+
+    st.markdown(f"[Source Code ](#SorceCode-1)")
 
 with col1:
     st.image("images/Photo.png",width=500)
 
+
 with col2:
+
     st.title("Prabhjot Singh Chhabra")
     content="""
-    Hello I am prabhjot i am learning python and solidity """
+    Hello I am prabhjot i am learning python and solidity.
+     With 4 years of experience in Python development, proficiency in C++ and SQL, 
+     strong foundation in programming & database management is built.
+   Have a solid understanding of machine learning algorithms and frameworks, including NumPy, PyTorch, and Matplotlib. Through my knowledge and experience with these tools, was able to develop and implement data-driven solutions that have improved business outcomes and increased efficiency."""
     st.info(content)
+
+st.write("Below You can find some of the apps I have built in Python. Feel free to contact me")
+
+col3,col4=st.columns(2)
+
+data=pd.read_csv("data.csv",sep=";")
+
+
+with col3:
+    for index,rows in data[0:10].iterrows():
+        structure(rows['title'],rows['description'],rows['image'],"ll")
+
+with col4:
+    for index, rows in data[10:].iterrows():
+            structure(rows['title'], rows['description'], rows['image'], "ll")
