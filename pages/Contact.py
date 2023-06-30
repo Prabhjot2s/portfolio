9d2678d7479ec984885fdb1fe65aca09142a9396
prabh <pschhabra22@gmail.com>
2023-06-30 01:40:40 -0400

    added requirement.txt file

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,16 +1,16 @@
-
-
 import streamlit as st
 from send_email import send_email
 st.header("Contact Me")
 
 with st.form(key='form'):
     emalfrom=st.text_input("Your Email address",placeholder="Enter your Email Address")
+    subject=st.selectbox(label='What do you want to discuss', options=['Job position', 'Freelancing', 'Other'])
 
     raw_content=st.text_area("Your message",placeholder="Happy to hear from you",height=300)
 
+
     content=f"""\
-Subject: New email from website
+Subject: New email from website regarding {subject}
 
 From: {emalfrom}
 {raw_content}
--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
-
 import smtplib,ssl
+import os
 
 def send_email(content):
     host='smtp.gmail.com'
     port=465
     context=ssl.create_default_context()
     username="pschhabra22@gmail.com"
-    passwords="tlwibzdkfqfmback"
+    passwords=os.getenv("PASSWORD")
 
     with smtplib.SMTP_SSL(host,port,context=context)as server:
         server.login(username,passwords)
